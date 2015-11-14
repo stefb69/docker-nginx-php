29609fe8e5c82ef57d05d83207d74c619954527a
root <root@moodle.cescan.org>
2015-11-13 22:29:35 -0500

    add script

--- start_web.sh
+++ start_web.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 reset=$1
+type=$2
 
+sed -i -e "s###" 
+ln -s /docker-nginx-php/build/conf/$type.conf /docker-nginx-php/build/conf/web.conf
 mkdir -p /var/www/web 
 
 if [ $reset == "true" ];then
